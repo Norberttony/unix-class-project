d141dd5da64607bcda27981bc264d683d9f91793
Norberttony <norberttony@icloud.com>
2025-03-15 23:03:52 -0400

    extract stats, .env for secrets

---
+++ extract.mjs
@@ -0,0 +1,27 @@
+
+import fs from "fs";
+import sysInfo from "systeminformation";
+import { config } from "dotenv";
+
+config({ path: ".env" });
+
+
+async function getStats(){
+    const cpu = await sysInfo.currentLoad();
+    const mem = await sysInfo.mem();
+    const disk = await sysInfo.disksIO();
+    const diskSpace = await sysInfo.fsSize();
+    const net = await sysInfo.networkStats();
+
+    return `cpu load=${cpu.currentLoad}
+mem usage=${mem.used / mem.total * 100}
+dsk usage=${diskSpace[0].use} total_wait_percent=${disk.tWaitPercent}
+net transfer=${net[0].tx_bytes} receive=${net[0].rx_bytes}`;
+}
+
+
+// record stats every 2 seconds
+while (true){
+    fs.writeFileSync(process.env.PIPE, await getStats());
+    await new Promise((res) => setTimeout(res, 2000));
+}
--- index.mjs
+++ index.mjs
@@ -1,8 +1,11 @@
 
+import fs from "fs";
 import pathModule from "path"
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+
+const PIPE_PATH = "/tmp/sysmon_pipe";
 
 
 export const app = express();
@@ -10,6 +13,10 @@ const server = http.createServer(app);
 
 export const io = new Server(server);
 
+
+fs.createReadStream(PIPE_PATH, { encoding: "utf8" }).on("data", (data) => {
+    io.emit("data", data.toString());
+});
 
 app.use(express.static(pathModule.resolve("./public")));
 
